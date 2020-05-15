aec4ae166a8ef3170ed1ece412fa5db7a5e97873
Wagenheim <wagenheimk@me.com>
2020-05-15 16:35:57 -0400

    added components for navbar and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import NavbarComponent from './components/NavbarComponent';
+import LoginComponent from './components/LoginComponent';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {User} from './models/users';
 
 function App() {
+
+  //@ts-ignore
+  const [authUser, setAuthUser] = useState(null as User);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+
+      <AppBar color = "primary" position = "static">
+
+        <Toolbar>                           
+
+          <Typography variant = "h5" color = "inherit">
+
+            <NavbarComponent />
+
+          </Typography>
+
+        </Toolbar>
+
+      </AppBar>
+
+      <Switch>
+
+        <Route path = "/login" render = {() => <LoginComponent authUser = {authUser} setAuthUser = {setAuthUser}/>}/>
+
+      </Switch>
+
+    </Router>
+    </>
   );
 }
 
