d1181ec31824529a528db9567574b887cf147bf7
Wagenheim <wagenheimk@me.com>
2020-05-17 16:26:31 -0400

    added logout buttons to navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {makeStyles, List, Typography, ListItem, ListItemText} from '@material-ui/core';
 import {Link} from 'react-router-dom';
+import { User } from '../models/users';
+import { reimbClient } from '../remote/reimb-client';
 
 interface INavBarProps{
 
-
+    authUser: User
+    setAuthUser: (user: User) => void
 
 }
 
@@ -17,9 +20,18 @@ const useStyles = makeStyles({
 
 });
 
+
 const NavbarComponent = (props: INavBarProps) => {
 
     const classes = useStyles();
+
+    async function logout(){
+
+        await reimbClient.get('/auth');
+        //@ts-ignore
+        props.setAuthUser(null as User);
+    
+    }
 
     return(
         <>
@@ -27,37 +39,80 @@ const NavbarComponent = (props: INavBarProps) => {
 
                 <ListItem component = "div">
 
-                    <Typography color = "inherit" variant = "h5">Reimbursments</Typography>
+                    <Typography color = "inherit" variant = "h5">ERS</Typography>
 
-                    <ListItemText inset>
-                        <Typography color = "inherit" variant = "h6">
-                            <Link to = "/home" className = {classes.link}>Home</Link>
-                        </Typography>
-                    </ListItemText>
+                    {
+                        !props.authUser ?
 
-                    <ListItemText inset>
-                        <Typography color = "inherit" variant = "h6">
-                            <Link to = "/login" className = {classes.link}>Login</Link>
-                        </Typography>
-                    </ListItemText>
+                            <ListItemText inset>
+                                <Typography color = "inherit" variant = "h6">
+                                    <Link to = "/login" className = {classes.link}>Login</Link>
+                                </Typography>
+                            </ListItemText> 
 
-                    <ListItemText inset>
-                        <Typography color = "inherit" variant = "h6">
-                            Register
-                        </Typography>
-                    </ListItemText>
+                        :
+                        
+                        props.authUser.roleId === 1 ?
+                            <>
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        <Link to = "/home" className = {classes.link}>Home</Link>
+                                    </Typography>
+                                </ListItemText>
 
-                    <ListItemText inset>
-                        <Typography color = "inherit" variant = "h6">
-                            <Link to = "/users" className = {classes.link}>Users</Link>
-                        </Typography>
-                    </ListItemText>
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        Register
+                                    </Typography>
+                                </ListItemText>
 
-                    <ListItemText inset>
-                        <Typography color = "inherit" variant = "h6">
-                            <Link to = "/reimbursments" className = {classes.link}>Reimbursments</Link>
-                        </Typography>
-                    </ListItemText>
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        <Link to = "/users" className = {classes.link}>Users</Link>
+                                    </Typography>
+                                </ListItemText>
+
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        <Link to = '' onClick = {logout} className = {classes.link}>Logout</Link>
+                                    </Typography>
+                                </ListItemText> 
+                            </>
+                        
+                        :
+
+                        props.authUser.roleId === 2 ?
+
+                            <>
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        <Link to = "/home" className = {classes.link}>Home</Link>
+                                    </Typography>
+                                </ListItemText>
+
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        <Link to = "/reimbursments" className = {classes.link}>Reimbursments</Link>
+                                    </Typography>
+                                </ListItemText>
+
+                                <ListItemText inset>
+                                    <Typography color = "inherit" variant = "h6">
+                                        <Link to = '' onClick = {logout} className = {classes.link}>Logout</Link>
+                                    </Typography>
+                                </ListItemText> 
+                            </>
+
+                        :
+
+                        <ListItemText inset>
+                            <Typography color = "inherit" variant = "h6">
+                                <Link to = "/home" className = {classes.link}>Home</Link>
+                            </Typography>
+                        </ListItemText>
+
+                    }
+
 
                 </ListItem>
 
