52b636f2b20b15eb2c361fbc1c566129fe5e0582
Wagenheim <wagenheimk@me.com>
2020-05-19 23:32:15 -0400

    changed baseUrl to test deploy

--- src/remote/reimb-client.ts
+++ src/remote/reimb-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const reimbClient = axios.create({
-    baseURL: 'http://localhost:8080',
-    //baseURL: 'http://kevinwagenheimproject1api-env.eba-pfzxxg4c.us-east-1.elasticbeanstalk.com',
+    //baseURL: 'http://localhost:8080',
+    baseURL: 'http://kevinwagenheimproject1api-env.eba-pfzxxg4c.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
     },
