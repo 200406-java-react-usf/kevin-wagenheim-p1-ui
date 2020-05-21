9b364df59895ef472270d8c5c01ad697a48adc36
Wagenheim <wagenheimk@me.com>
2020-05-21 12:05:54 -0400

    tested app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {mount} from 'enzyme';
+import NavbarComponent from './components/NavbarComponent';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('testing main app component', () => {
+
+  test('renders App component', () => {
+    
+    const app = render(<App/>);
+    expect(app).toBeTruthy();
+
+  });
+
+  test('renders navbar component', () => {
+
+    const wrapper = mount(<App/>);
+
+    expect(wrapper.find(NavbarComponent)).toHaveLength(1);
+
+  });
+
 });
